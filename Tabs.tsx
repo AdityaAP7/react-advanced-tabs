7b28872ce738cfdf448c39a70cf95045dc939dff
AdityaAP7 <adityapalodkar@gmail.com>
2025-01-06 22:57:58 +0530

    Active index logic

--- Tabs.tsx
+++ Tabs.tsx
@@ -1,2 +1,7 @@
 // src/components/Tabs.tsx
 import React, { createContext, useState, ReactNode } from 'react';
+
+interface TabsContextProps {
+    activeIndex: number;
+    setActiveIndex: (index: number) => void;
+  }
