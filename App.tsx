f6543cfab66cb1993461b26ffb3bb938298a2b32
AdityaAP7 <adityapalodkar@gmail.com>
2025-01-07 21:32:58 +0530

    Added app.tsx logic

--- App.tsx
+++ App.tsx
@@ -7,4 +7,4 @@ import {
     TabPanel
   } from 'react-advanced-tabs';  return (
     <div style={{ width: '600px', margin: '0 auto' }}>
-         <Tabs defaultIndex={0}> <TabList><Tab index={0} activeClassName="active">Home</Tab><Tab index={1} activeClassName="active">Profile</Tab> <Tab index={2} activeClassName="active">Settings</Tab> </TabList><TabPanel index={0}>
+         <Tabs defaultIndex={0}> <TabList><Tab index={0} activeClassName="active">Home</Tab><Tab index={1} activeClassName="active">Profile</Tab> <Tab index={2} activeClassName="active">Settings</Tab> </TabList><TabPanel index={0}> <h2>Home Content</h2>
