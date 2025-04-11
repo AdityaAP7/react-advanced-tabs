4d3828dba8765a5836e8c22acf91619b54205867
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-04-11 23:15:09 +0530

    Update Tabs.tsx

--- Tabs.tsx
+++ Tabs.tsx
@@ -1,6 +1,6 @@
 // src/components/Tabs.tsx
 import React, { createContext, useState, ReactNode } from 'react';
-12345678
+123456789
 interface TabsContextProps {
     activeIndex: number;
     setActiveIndex: (index: number) => void;
