26d821baca1f4e40da83ce75c3b13c83f1cf7537
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-05-14 23:55:56 +0530

    Update TabList.tsx

--- TabList.tsx
+++ TabList.tsx
@@ -1,4 +1,4 @@
-321987665432108987654321// src/components/TabList.tsx
+4321987665432108987654321// src/components/TabList.tsx
 import React, { ReactNode } from 'react';
 interface TabListProps {
     children: ReactNode;
