de453346c1a25fbc61c583b87d6770a79498ccfc
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-05-30 23:42:59 +0530

    Update TabPanel.tsx

--- TabPanel.tsx
+++ TabPanel.tsx
@@ -1,4 +1,4 @@
-654321// src/components/TabPanel.tsx
+7654321// src/components/TabPanel.tsx
 import React, { useContext } from 'react';
 import { TabsContext } from './Tabs';
 interface TabPanelProps {
