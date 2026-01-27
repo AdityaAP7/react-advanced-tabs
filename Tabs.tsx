aa070aef6c6e3be86a4a1302c55558fef2675963
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2026-01-27 20:22:00 +0530

    Update Tabs.tsx

--- Tabs.tsx
+++ Tabs.tsx
@@ -1,4 +1,4 @@
-876543210987654321// src/components/Tabs.tsx
+9876543210987654321// src/components/Tabs.tsx
 import React, { createContext, useState, ReactNode } from 'react';
 1234567890qwertyuiop
 interface TabsContextProps {
